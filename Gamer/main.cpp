28406856cd42406b4a818946f02c0f554f3a4b68
Regan <reganlussier@gmail.com>
2018-10-13 00:48:33 -0500

    branch-1

---
+++ Gamer/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Gamer
+//
+//  Created by rlussier on 10/13/18.
+//  Copyright © 2018 admin16. All rights reserved.
+//
+
+#include <iostream>
+
+const int mapwidth = 120;
+const int mapheight = 120;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
+
