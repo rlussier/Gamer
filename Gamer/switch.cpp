4dd0574ea981e4f5ea8ddb2143700a798a7930cd
Reggie <reganlussier@gmail.com>
2018-10-29 10:57:38 -0500

    Update switch.cpp

--- Gamer/switch.cpp
+++ Gamer/switch.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    
+    int value = 4;
+    
+    switch (value) {
+        case 4:
+        cout << "Value is 4." << endl;
+        break;
+        case 5:
+        cout << "Value is 5." << endl;
+        break;
+        case 6:
+        cout << "Value is 6." << endl;
+        break;
+        default:
+        cout << "Unrecongnized value." << endl;
+    }
+	return 0;
+}
