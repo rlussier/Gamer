938aa1083728dfefbf33df63618f50e026562aee
Reggie <reganlussier@gmail.com>
2018-10-31 15:31:20 -0500

    Create Cat.cpp

---
+++ Gamer/Classes/src/Cat.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Cat.h"
+
+using namespace std;
+
+void speak() {
+    coout << "Meow!" << endl;
+}
+
+void jump() {
+    cout << "Jumping to top of bookcase" << endl;
+}
+
+int main() {
+	return 0;
+}
