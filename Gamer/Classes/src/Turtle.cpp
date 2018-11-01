fbdbc65e0bf94e3dade8dad00db21a99a469d383
Reggie <reganlussier@gmail.com>
2018-10-31 21:19:28 -0500

    Update and rename Cat.cpp to Turtle.cpp

---
+++ Gamer/Classes/src/Turtle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Turtle.h"
+
+using namespace std;
+
+void Turtle::speak() {
+    cout << "Hello!" << endl;
+}
+
+void Turtle::jump() {
+    cout << "Jumping into the water" << endl;
+}
+
+int main() {
+	return 0;
+}
