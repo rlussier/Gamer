63cf3edde7c7456b846c2ba5d004c73749c2d08d
Reggie <reganlussier@gmail.com>
2018-11-05 15:17:40 -0600

    Create Person.h

---
+++ Gamer/Constructor Parameters and Overloading/Person.h
@@ -0,0 +1,16 @@
+#infdef PERSON_H_
+#define PERSON_H_
+
+#include <iostream>
+
+using namespace std;
+
+class Person {
+  private:
+    string name;
+    int age;
+  public:
+    person();
+};
+
+#endif /* PERSON_H_ */
