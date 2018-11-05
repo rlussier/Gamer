25e39b9b20103423f3d557ab1ff25350b1ba1cfb
Reggie <reganlussier@gmail.com>
2018-11-05 12:56:09 -0600

    Create Person.h

---
+++ Gamer/Getters and Setters/src/Person.h
@@ -0,0 +1,18 @@
+* Person.h
+
+#ifndef PERSON_H_
+#define PERSON_H_
+
+#include <iostream>
+
+using namespace std;
+
+class Person {
+private: 
+  string name;
+
+public: 
+  Person();
+};
+
+#endif /*PERSON_H_*/
