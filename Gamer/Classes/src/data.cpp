7e30e9be834b08ec2684e38493debeea3110bf2b
Reggie <reganlussier@gmail.com>
2018-11-04 23:34:39 -0600

    Create data.cpp

---
+++ Gamer/Classes/src/data.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Turtle.h"
+
+using namespace std;
+
+int main() {
+    Turtle Kuvira;
+    Kuvira.happy = false;
+    Kuvira.speak();
+    
+    Turtle Berus;
+    Berus.happy = true;
+    Berus.speak();
+    
+	return 0;
+}
