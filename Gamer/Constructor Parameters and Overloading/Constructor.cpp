6b00a5db6df6b3e0b9a42c2ad76543bb32bd172d
Reggie <reganlussier@gmail.com>
2018-11-05 15:44:12 -0600

    Update Constructor.cpp

--- Gamer/Constructor Parameters and Overloading/Constructor.cpp
+++ Gamer/Constructor Parameters and Overloading/Constructor.cpp
@@ -7,7 +7,7 @@
   
   Person person1;
   Person person2("Berus");
-    Person person2("Lexi", 17);
+  Person person2("Lexi", 17);
   
   cout << person1.toString(); << endl; 
   cout << person2.toString(); << endl; 
