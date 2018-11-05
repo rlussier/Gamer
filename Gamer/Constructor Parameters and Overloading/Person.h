cc06006e344fc864257164860d8bb8b5b799052f
Reggie <reganlussier@gmail.com>
2018-11-05 15:40:46 -0600

    Update Person.h

--- Gamer/Constructor Parameters and Overloading/Person.h
+++ Gamer/Constructor Parameters and Overloading/Person.h
@@ -12,7 +12,7 @@ class Person {
   public:
     Person();
     Person(string newName){ name = newName; age = 0; };
-    Person(string newName, int age)
+    Person(string newName, int newAge)
     string toString();
 };
 
