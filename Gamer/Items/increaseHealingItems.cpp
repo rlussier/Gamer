f94f1290e729f77ef6687674bec95897069d912c
Reggie <reganlussier@gmail.com>
2018-11-30 12:50:29 -0600

    Create increaseHealingItems.cpp

---
+++ Gamer/Items/increaseHealingItems.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+    
+using namespace std;
+    
+int main() 
+{
+ double healingPotion;
+ double healingBerries;
+    
+ cout << "You increased your bag by: " << endl;
+ cin >> healingPotion * healingBerries;
+   
+ return 0; 
+}
