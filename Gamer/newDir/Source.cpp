270fe51e3280f3ebd0dca5d8c11843269f99ba3a
Reggie <reganlussier@gmail.com>
2018-12-13 12:54:10 -0600

    Create Source.cpp

---
+++ Gamer/newDir/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int username;
+  cout << "What is your username?";
+  cin << username;
+  
+  if(username == true) {
+  cout << "Welcome back!";
+  }
+  
+  return 0;
+}
